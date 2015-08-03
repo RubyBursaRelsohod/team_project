49694572b451756d4837890f01f20770f775ea0a
Volkov, Evgeny <evolkov@gmail.com>
2015-08-03 11:25:45 -0400

    Hound check fix

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,11 +1,10 @@
 FactoryGirl.define do
   factory :product do
     name "MyString"
-category_id 1
-price 1.5
-country "MyString"
-company "MyString"
-quantity 1
+    category_id 1
+    price 1.5
+    country "MyString"
+    company "MyString"
+    quantity 1
   end
-
 end
--- spec/factories/products_categories.rb
+++ spec/factories/products_categories.rb
@@ -2,5 +2,4 @@ FactoryGirl.define do
   factory :products_category do
     name "MyString"
   end
-
 end
