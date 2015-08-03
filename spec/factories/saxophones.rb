40b41381dfd016f5912fd00f761be2f5a68f71df
Dioscur <dioscurk@gmail.com>
2015-08-04 00:12:22 +0300

    Check fix

--- spec/factories/saxophones.rb
+++ spec/factories/saxophones.rb
@@ -1,13 +1,12 @@
 FactoryGirl.define do
   factory :saxophone do
     type ""
-tonality "MyString"
-keys_number 1
-low_key "MyString"
-high_key "MyString"
-bell_diametr 1.5
-bell_engraved false
-body_material "MyString"
+    tonality "MyString"
+    keys_number 1
+    low_key "MyString"
+    high_key "MyString"
+    bell_diametr 1.5
+    bell_engraved false
+    body_material "MyString"
   end
-
 end
