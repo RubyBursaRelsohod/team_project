5043a6167b2cefda1a29dcd5b99ff5bfa303ddaa
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-08-04 19:05:52 +0300

    Consistency and Hound improvements

--- db/migrate/20150804153457_add_other_references.rb
+++ db/migrate/20150804153457_add_other_references.rb
@@ -1,8 +1,6 @@
 class AddOtherReferences < ActiveRecord::Migration
   def change
-  	remove_column :orders, :user_id
     remove_column :orders, :status_id
-    remove_column :pianos, :product_id
 
     add_reference :pianos, :products, index: true, foreign_key: true
     add_reference :orders, :users, index: true, foreign_key: true
