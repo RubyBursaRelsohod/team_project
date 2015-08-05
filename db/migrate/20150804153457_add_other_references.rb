f69daf7a0609cc097384229bd9aa504f6a8335ff
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-08-05 14:50:21 +0300

    Changed references

--- db/migrate/20150804153457_add_other_references.rb
+++ db/migrate/20150804153457_add_other_references.rb
@@ -2,7 +2,7 @@ class AddOtherReferences < ActiveRecord::Migration
   def change
     remove_column :orders, :status_id
 
-    add_reference :pianos, :products, index: true, foreign_key: true
-    add_reference :orders, :users, index: true, foreign_key: true
+    # add_reference :pianos, :product, index: true, foreign_key: true
+    # add_reference :orders, :user, index: true, foreign_key: true
   end
 end
--- db/migrate/20150804155150_remove_column_user_id_from_orders.rb
+++ db/migrate/20150804155150_remove_column_user_id_from_orders.rb
@@ -1,6 +1,6 @@
 class RemoveColumnUserIdFromOrders < ActiveRecord::Migration
   def change
-    remove_column :orders, :user_id
-    remove_column :pianos, :product_id
+    # remove_column :orders, :users_id
+    # remove_column :pianos, :products_id
   end
 end
