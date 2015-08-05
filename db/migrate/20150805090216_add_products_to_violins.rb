eddfe612e4336ff18d5a3a3860ec46033f569c8e
Serg1i <sergii.manannikov@hotmail.com>
2015-08-05 15:35:54 +0300

    fix  plural column name to singular

--- db/migrate/20150805090216_add_products_to_violins.rb
+++ db/migrate/20150805090216_add_products_to_violins.rb
@@ -1,5 +1,5 @@
 class AddProductsToViolins < ActiveRecord::Migration
   def change
-    add_reference :violins, :products, index: true, foreign_key: true
+    add_reference :violins, :product, index: true, foreign_key: true
   end
 end
--- db/migrate/20150805090539_add_products_to_orders_products.rb
+++ db/migrate/20150805090539_add_products_to_orders_products.rb
@@ -1,5 +1,5 @@
 class AddProductsToOrdersProducts < ActiveRecord::Migration
   def change
-    add_reference :orders_products, :products, index: true, foreign_key: true
+    add_reference :orders_products, :product, index: true, foreign_key: true
   end
 end
