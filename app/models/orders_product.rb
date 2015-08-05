4bd854e3f03e0cf058c0aad86b024624d1e18274
Serg1i <sergii.manannikov@hotmail.com>
2015-08-05 17:19:12 +0300

    added relations

--- app/models/orders_product.rb
+++ app/models/orders_product.rb
@@ -1,2 +1,4 @@
 class OrdersProduct < ActiveRecord::Base
+  has_one : product
+  belongs_to : order
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   has_one :guitar
   has_one :saxophone
   has_one :piano
+  has_one :violin
   has_many :comments
   belongs_to :products_category
 end
