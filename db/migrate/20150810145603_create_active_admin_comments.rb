14c8ab4a3bebd4e1ece38f81dae950435c54778f
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-08-10 18:14:17 +0300

    Hound improvements

--- db/migrate/20150810145603_create_active_admin_comments.rb
+++ db/migrate/20150810145603_create_active_admin_comments.rb
@@ -2,8 +2,8 @@ class CreateActiveAdminComments < ActiveRecord::Migration
   def self.up
     create_table :active_admin_comments do |t|
       t.string :namespace
-      t.text   :body
-      t.string :resource_id,   null: false
+      t.text :body
+      t.string :resource_id, null: false
       t.string :resource_type, null: false
       t.references :author, polymorphic: true
       t.timestamps
