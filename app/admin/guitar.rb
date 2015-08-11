659e9bdec46c9bcbae861bf7ade76c806412076b
Volkov, Evgeny <evolkov@gmail.com>
2015-08-11 07:10:40 -0400

    Fix codestyle 6

--- app/admin/guitar.rb
+++ app/admin/guitar.rb
@@ -1,4 +1,4 @@
 ActiveAdmin.register Guitar do
-  permit_params :frets, :chords, :dimensions, :weight, :is_digital, 
+  permit_params :frets, :chords, :dimensions, :weight, :is_digital,
       :created_at, :updated_at, :product_id
 end
--- app/admin/saxophone.rb
+++ app/admin/saxophone.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Saxophone do
-  permit_params :type, :tonality, :keys_number, :low_key, :high_key, 
-      :bell_diametr, :bell_engraved, :body_material, :created_at, :updated_at, :product_id
+  permit_params :type, :tonality, :keys_number, :low_key, :high_key,
+      :bell_diametr, :bell_engraved, :body_material, :created_at, :updated_at,
+      :product_id
 end
