c0c400d7c0b183453aadf3a2ecf97f6bfe4f609c
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-08-11 16:55:54 +0300

    Removed unnecessary pending sections

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
   describe "validations" do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
--- spec/models/violin_spec.rb
+++ spec/models/violin_spec.rb
@@ -1,8 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Violin, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-
   describe "validations" do
     it { should validate_presence_of :color }
     it { should validate_presence_of :bow_included }
