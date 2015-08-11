42bf8788b69e0b5c97a3974733646ce6c0944612
Serg1i <sergii.manannikov@hotmail.com>
2015-08-11 16:18:35 +0300

    fix idents, spaces and other  hound issues

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,7 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
-
+  # pending "add some examples to (or delete) #{__FILE__}"
   describe "validations" do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
@@ -10,18 +9,13 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :email }
     it { should validate_presence_of :phone }
     it { should validate_presence_of :address }
-
     it { should allow_value("John").for(:first_name) }
     it { should_not allow_value("jOhn").for(:first_name) }
     it { should allow_value("Smith").for(:last_name) }
     it { should_not allow_value("sMith").for(:last_name) }
     it { should allow_value("JS@example.com").for(:email) }
-
   end
-
   describe "associations" do
     it { should have_many :comments }
   end
-
-
 end
