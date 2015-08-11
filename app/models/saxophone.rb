c098e1574beab47932268d322a81a3fdf3fa7449
Dioscur <dioscurk@gmail.com>
2015-08-11 14:06:01 +0300

    Add specs and validations for Saxophone, Product, ProductCategory

--- app/models/saxophone.rb
+++ app/models/saxophone.rb
@@ -1,3 +1,10 @@
 class Saxophone < ActiveRecord::Base
   belongs_to :product
+
+  validates :product_id, presence: true, numericality: { greater_than: 0,
+                                                         only_integer: true }
+  validates :bell_diametr, presence: true
+  validates :type, :tonality, :low_key, :high_key, presence: true
+  validates_numericality_of :bell_diametr
+
 end
--- spec/models/saxophone_spec.rb
+++ spec/models/saxophone_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
 RSpec.describe Saxophone, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+    # it { should validate_presence_of :product_id }
+    it { should validate_presence_of :type }
+    it { should validate_presence_of :tonality }
+    it { should validate_presence_of :bell_diametr }
+    it { should validate_presence_of :low_key }
+    it { should validate_presence_of :high_key }
+
+    # it { should validate_numericality_of :product_id }
+
+    it { should allow_value("10.11").for(:bell_diametr) }
+    it { should_not allow_value("green").for(:bell_diametr) }
+    it { should_not allow_value("10.11").for(:product_id) }
+  end
+
+  describe "associations" do
+    it { should belong_to :product }
+  end
 end
