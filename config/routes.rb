f3e3680700f0320a237fe1a92a0373c7c84019c2
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-08-19 15:36:48 +0300

    Add Ajax pagination to Violins

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   end
 
   resources :saxophones, concerns: :paginatable
+  resources :violins, concerns: :paginatable
 end
---
+++ spec/controllers/violins_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ViolinsController, type: :controller do
+
+end
