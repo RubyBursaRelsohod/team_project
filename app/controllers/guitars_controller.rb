c5b30416073e29678dfded197d39e11edaa3a335
AntonDp <ant1099@i.ua>
2015-08-19 22:47:58 +0300

    asgasdgasg

---
+++ app/controllers/guitars_controller.rb
@@ -0,0 +1,13 @@
+class GuitarsController < ApplicationController
+  def index
+    @guitars = Product.every(:Guitar)
+               .order('id')
+               .page(params[:page])
+               .per(10)
+
+    respond_to do |format|
+      format.html
+      format.js {}
+    end
+  end
+end
---
+++ app/helpers/guitar_helper.rb
@@ -0,0 +1,2 @@
+module GuitarHelper
+end
