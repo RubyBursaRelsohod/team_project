307366abebac9b06f6a8ef4f4ecd188ae74704bb
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-08-20 18:10:17 +0300

    Mock up user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'profile/index'
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root "test#index"
