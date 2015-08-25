f311561af3c215cea6d773b0f27fce5ddf2dee4f
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-08-25 21:16:07 +0300

    Upload about page for review

--- app/assets/javascripts/components/constants.es6.jsx
+++ app/assets/javascripts/components/constants.es6.jsx
@@ -4,7 +4,20 @@ const Сonsts = ( () => {
 
     const description = {
         title: 'Welcome to the Symphony Store!',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe aut sint nobis quia error ut, ex, modi, nihil cum consequuntur quis dicta ab! Aperiam delectus rem, at eaque culpa repellat dolorum dignissimos iste, placeat et incidunt nesciunt doloribus deserunt id cumque nulla quos perferendis. Enim, excepturi! Sapiente consectetur, accusantium nesciunt aspernatur officia corporis impedit iure earum maxime deleniti quod. Debitis illum repellat officiis, molestias, laudantium consectetur eligendi! Voluptas iste tempore consectetur, magnam quisquam pariatur quas ab, accusantium error architecto quam explicabo distinctio ipsam! Omnis numquam facere facilis, quam magni ex optio illum. Nam alias inventore autem. Sit qui quasi maiores.'
+        text: [
+            {
+                subtitle: 'About the Store',
+                body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe aut sint nobis quia error ut, ex, modi, nihil cum consequuntur quis dicta ab! Aperiam delectus rem, at eaque culpa repellat dolorum dignissimos iste, placeat et incidunt nesciunt doloribus deserunt id cumque nulla quos perferendis. Enim, excepturi! Sapiente consectetur, accusantium nesciunt aspernatur officia corporis impedit iure earum maxime deleniti quod. Debitis illum repellat officiis, molestias, laudantium consectetur eligendi! Voluptas iste tempore consectetur, magnam quisquam pariatur quas ab, accusantium error architecto quam explicabo distinctio ipsam! Omnis numquam facere facilis, quam magni ex optio illum. Nam alias inventore autem. Sit qui quasi maiores.'
+            },
+            {
+                subtitle: 'Delivery Info',
+                body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam numquam consectetur animi autem voluptatum sapiente praesentium perspiciatis ipsam, vel ullam quisquam distinctio, sit soluta iure error nobis enim labore fugiat voluptatem officiis? Cupiditate ipsam ex blanditiis adipisci, aliquid sapiente asperiores.'
+            },
+            {
+                subtitle: 'Customer Success Philosophy',
+                body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet recusandae saepe, assumenda provident esse veritatis aspernatur laborum, tenetur et, placeat, ut accusantium sit. Possimus exercitationem at amet vitae voluptatem commodi. Molestiae soluta placeat error. Repellat, vero! Nemo enim quae quisquam accusamus corporis nulla magni iusto possimus, velit, in est tenetur.'
+            }
+        ]
     }
 
     const mockQuestions = [
--- app/assets/javascripts/components/description.es6.jsx
+++ app/assets/javascripts/components/description.es6.jsx
@@ -1,17 +1,27 @@
 class Description extends React.Component {
   render () {
     const desc = Сonsts.description;
+    let articles = desc.text.map((item, index) => {
+      return (
+        <div className="subarticle" key={index}>
+          <h3>{item.subtitle}</h3>
+          <article lang="en">
+            <p>{item.body}</p>
+          </article>
+        </div>
+      );
+    });
+
     return (
-      <div>
+      <div className="description">
         <div>
           <img className="img-responsive"
                src="http://i.imgur.com/vP9pOJi.jpg" />
           <h1>{desc.title}</h1>
         </div>
         <br />
-        <article lang="en">
-          <p>{desc.text}</p>
-        </article>
+        {articles}
+        <hr />
       </div>
     );
   }
