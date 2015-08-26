72513d453234024c3efa9a6da9137b35b78931e0
Volkov, Evgeny <evolkov@gmail.com>
2015-08-26 07:30:27 -0400

    Fix AWS S3 Credentials

--- app/models/products_photo.rb
+++ app/models/products_photo.rb
@@ -14,7 +14,6 @@
 # ProductsPhoto model
 class ProductsPhoto < ActiveRecord::Base
   belongs_to :product
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, s3_credentials: "#{Rails.root}/config/environments/s3.yml"
-
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
