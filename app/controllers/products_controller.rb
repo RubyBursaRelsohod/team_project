14345cdfb9d48ef6a565790a759fa6850e08031d
Volkov, Evgeny <evolkov@gmail.com>
2015-08-30 19:53:50 -0400

    Add Sounds for views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,15 +2,16 @@ class ProductsController < ApplicationController
   def show
     product = Product.find(params[:id])
     photos = ProductsPhoto.where(product: product)
+    sounds = ProductsSound.where(product: product)
     case product.products_category.name
     when 'Violin'
-      @product = [product, Violin.where(product: product).first, photos]
+      @product = [product, Violin.where(product: product).first, photos, sounds]
     when 'Guitar'
-      @product = [product, Guitar.where(product: product).first, photos]
+      @product = [product, Guitar.where(product: product).first, photos, sounds]
     when 'Saxophone'
-      @product = [product, Saxophone.where(product: product).first, photos]
+      @product = [product, Saxophone.where(product: product).first, photos, sounds]
     when 'Piano'
-      @product = [product, Piano.where(product: product).first, photos]
+      @product = [product, Piano.where(product: product).first, photos, sounds]
     end
     @comment = Comment.new
     @comments = Comment.where(product_id: product.id).order('created_at DESC')
